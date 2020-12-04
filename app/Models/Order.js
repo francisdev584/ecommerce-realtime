2b96fc7e475e28d305fcd784da50e227d9053d35
Francinildo M. Rodrigues <francis584@gmail.com>
2020-12-04 14:11:09 -0300

    fix OrderHook.updateCollectionValues

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -8,6 +8,7 @@ class Order extends Model {
     super.boot()
 
     this.addHook('afterFind','OrderHook.updateValues')
+    this.addHook('afterPaginate','OrderHook.updateCollectionValues')
   }
 
   items() {
