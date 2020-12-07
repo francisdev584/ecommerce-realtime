e44b25a91e672fb218ba05f1184e71a974b56c9a
Francinildo M. Rodrigues <francis584@gmail.com>
2020-12-07 18:07:00 -0300

    refresh_token method created

--- app/Controllers/Http/auth/AuthController.js
+++ app/Controllers/Http/auth/AuthController.js
@@ -36,7 +36,17 @@ class AuthController {
   }
 
   async refresh({ request, response, auth }){
-    //
+    let refresh_token = request.input('refresh_token')
+
+    if(!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+
+    const user = await auth
+    .newRefreshToken()
+    .generateForRefreshToken(refresh_token)
+
+    return response.send({ data: user })
 
   }
 
