726cae9ade684a3586bec45197b7ab87fbb0312c
Francinildo M. Rodrigues <francis584@gmail.com>
2020-12-08 14:12:40 -0300

    Category.update

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -78,7 +78,16 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params: {id}, request, response }) {
+    const category = await Category.findOrFail(id)
+
+    const {title, description, image_id} = request.all()
+
+    category.merge({title, description, image_id})
+
+    await category.save()
+
+    return response.send(category)
   }
 
   /**
