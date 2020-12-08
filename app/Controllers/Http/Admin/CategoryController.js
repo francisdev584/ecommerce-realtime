7b801351d0f179719ac10b3f83c54496adfe07a7
Francinildo M. Rodrigues <francis584@gmail.com>
2020-12-08 14:08:58 -0300

    Category.destroy done

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -89,7 +89,12 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: {id}, request, response }) {
+    const category = await Category.findOrFail(id)
+
+    await category.delete()
+
+    return response.status(204).send()
   }
 }
 
