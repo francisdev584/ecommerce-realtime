e46774a0f059227c83299ddbe521ee0724fb4d4a
Francinildo M. Rodrigues <francis584@gmail.com>
2020-12-09 11:43:42 -0300

    CouponController.show

--- app/Controllers/Http/Admin/CouponController.js
+++ app/Controllers/Http/Admin/CouponController.js
@@ -51,7 +51,10 @@ class CouponController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params: {id}, request, response }) {
+    const coupon = await Coupon.findOrFail(id)
+
+    return response.send(coupon)
   }
 
   /**
