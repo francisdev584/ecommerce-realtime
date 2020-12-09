ba0a5007c0e84eacd13d8b7ac42fb4c6e8c9dd45
Francinildo M. Rodrigues <francis584@gmail.com>
2020-12-09 10:21:39 -0300

    ImageController.store image and images

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -8,6 +8,7 @@
  * Resourceful controller for interacting with images
  */
 const Image = use('App/Models/Image')
+const { manage_single_upload, manage_multiple_uploads } = use('App/Helpers')
 class ImageController {
   /**
    * Show a list of all images.
@@ -35,6 +36,60 @@ class ImageController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+    try {
+      // captura uma imagem ou mais do request
+      const fileJar = request.file('images', {
+        type: ['image'],
+        size: '2mb'
+      })
+
+      // retorno pro usuario
+      let images = []
+
+      // caso seja um único arquivo - manage_single_upload
+      if (!fileJar.files) {
+        const file = await manage_single_upload(fileJar)
+        if (file.moved()) {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+
+          images.push(image)
+
+          return response.status(201).send({ successes: images, errors: {} })
+        }
+
+        return response.status(400).send({
+          message: 'Não foi possível processar esta imagem no momento!'
+        })
+      }
+      // caso sejam vários arquivos - manage_multiple_uploads
+      let files = await manage_multiple_uploads(fileJar)
+
+      await Promise.all(
+        files.successes.map(async file => {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+          images.push(image)
+        })
+      )
+
+      return response.status(201).send({
+        successes:images,
+        errors: files.errors
+      })
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possível processar sua solicitação'
+      })
+    }
   }
 
   /**
