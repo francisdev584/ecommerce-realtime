d20048a35d3f77c72554d82a323b68998d0fc320
Francinildo M. Rodrigues <francis584@gmail.com>
2020-12-09 09:22:12 -0300

    fix updated Buffer function

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -14,9 +14,9 @@ const str_random = async ( length =40 ) => {
   let len = string.length
 
   if (len < length) {
-    let size = lenth - len
+    let size = length - len
     let bytes = await crypto.randomBytes(size)
-    let buffer = new Buffer(bytes)
+    let buffer = Buffer.from(bytes)
     string += buffer.toString('base64').replace(/[^a-zA-Z0-9]/g,'').substr(0, size)
   }
 
