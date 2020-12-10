e96fde2c0a5a59b46d03703db936eda473fc5513
Francinildo M. Rodrigues <francis584@gmail.com>
2020-12-10 11:01:01 -0300

    OrderService.syncItems and UpdateItems

---
+++ app/Services/Order/OrderService.js
@@ -0,0 +1,37 @@
+'use strict'
+
+class OrderService {
+  constructor(model, transaction = false) {
+    this.model = model
+    this.transaction = transaction
+  }
+
+  async syncItems(items) {
+    if(!Array.isArray(items)) {
+      return false
+    }
+    await this.model.items().delete(this.transaction)
+    await this.model.items().createMany( [ items, this.transaction ] )
+  }
+
+  async updateItems(items) {
+    let currentItems = await this.model
+    .items()
+    .whereIn('id', items.map(item => item.id))
+    .fetch()
+    // deleta os items que o user não quer mais
+    await this.model
+    .items()
+    .whereNotIn('id', items.map(item => item.id))
+    .delete(this.transaction)
+
+    // Atualiza os valores e quantidades
+    await Promise.all(
+      currentItems.rows.map(async item => {
+        item.fill(items.find(n => n.id === item.id))
+
+        await item.save(this.transaction)
+      })
+    )
+  }
+}
