74a4bb86533b8b6c7ec36f94517562350eb12d02
Francinildo M. Rodrigues <francis584@gmail.com>
2020-12-14 19:47:22 -0300

    category trans for update

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -86,14 +86,16 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params: {id}, request, response }) {
-    const category = await Category.findOrFail(id)
+  async update ({ params: {id}, request, response, transform }) {
+    const categoryData = await Category.findOrFail(id)
 
     const {title, description, image_id} = request.all()
 
-    category.merge({title, description, image_id})
+    categoryData.merge({title, description, image_id})
 
-    await category.save()
+    await categoryData.save()
+
+    const category = await transform.item(categoryData, categoryTransformer)
 
     return response.send(category)
   }
