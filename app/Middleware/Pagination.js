b48c78675148e94922bfb87840c434559bc6b96c
Francinildo M. Rodrigues <francis584@gmail.com>
2020-12-16 10:26:23 -0300

    fix default on Pagination middleware

--- app/Middleware/Pagination.js
+++ app/Middleware/Pagination.js
@@ -12,8 +12,8 @@ class Pagination {
   async handle (ctx, next) {
     // call next to advance the request
     if(ctx.request.method() === 'GET') {
-      const page = parseInt(ctx.request.input('page'))
-      const limit = parseInt(ctx.request.input('limit'))
+      const page = ctx.request.input('page') ? parseInt(ctx.request.input('page')) : 1
+      const limit = ctx.request.input('limit') ? parseInt(ctx.request.input('limit')) : 20
       const perpage = parseInt(ctx.request.input('perpage'))
 
       ctx. pagination = {
